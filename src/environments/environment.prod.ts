457316bbbaac7ff0158b6114c62b3b894c0d786e
GabrielGCSOARES <126501271+GabrielGCSOARES@users.noreply.github.com>
2024-11-22 18:00:41 +0000

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyAgqpnXNxZ8abif8ssQq_C2s998w7jZb68",
-    authDomain: "api-aula-d6451.firebaseapp.com",
-    projectId: "api-aula-d6451",
-    storageBucket: "api-aula-d6451.appspot.com",
-    messagingSenderId: "48648388488",
-    appId: "1:48648388488:web:dc73e0a1496a52cd4b843d",
-    measurementId: "G-CR3MDWMWJF"
+    apiKey: "AIzaSyArY9z9-6inXt0rc6J2chluKNz09Lw9cxc",
+    authDomain: "aula-tomas-turbano.firebaseapp.com",
+    projectId: "aula-tomas-turbano",
+    storageBucket: "aula-tomas-turbano.appspot.com",
+    messagingSenderId: "126511705678",
+    appId: "1:126511705678:web:bf3ae88fa5be5133a0dd1f",
+    measurementId: "G-7MM9FEJ7SR"
   },
   production: true
 };
